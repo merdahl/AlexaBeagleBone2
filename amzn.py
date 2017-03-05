cdf107ddeb9ca4f11ca5e5af59e196e752e79841
Mike Erdahl <m-erdahl@ti.com>
2017-03-05 14:10:14 -0600

    Put AVS Request Header into separate module
    Signed-off-by: Mike Erdahl <m-erdahl@ti.com>

---
+++ amzn.py
@@ -0,0 +1,20 @@
+avsRequestHeader = {
+        "messageHeader": {
+            "deviceContext": [
+                {
+                    "name": "playbackState",
+                    "namespace": "AudioPlayer",
+                    "payload": {
+                        "streamId": "",
+                        "offsetInMilliseconds": "0",
+                        "playerActivity": "IDLE"
+                    }
+                }
+            ]
+        },
+        "messageBody": {
+            "profile": "alexa-close-talk",
+            "locale": "en-us",
+            "format": "audio/L16; rate=16000; channels=1"
+        }
+    }
